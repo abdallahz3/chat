a86e70d88c2e3a23a7208ab1dd64e3d166ee81d5
3bdallahz <azsahhar@gmail.com>
2019-05-22 18:16:45 +0300

    docker

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :chat, ChatWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "staging.cense.ai", port: 80],
+  url: [host: "staging.cense.ai", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
